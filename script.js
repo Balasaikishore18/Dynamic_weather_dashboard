dba644273814354cf441bb56a4ba7986d1ae13bf
Balasai kishore <163674191+Balasaikishore18@users.noreply.github.com>
2025-09-30 14:13:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ** IMPORTANT: Replace 'YOUR_OPENWEATHERMAP_API_KEY' with your actual key **
-const API_KEY = '';
+const API_KEY = 'YOUR_OPENWEATHERMAP_API_KEY';
 const WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 const FORECAST_BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
